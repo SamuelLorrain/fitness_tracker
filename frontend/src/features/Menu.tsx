07de911ebf67f67a16b23a8d05d51ac975e1f864
SamuelLorrain <samuel.lorrain@gmail.com>
2024-04-27 01:45:24 +0200

    feat: create menu

--- frontend/src/features/Menu.tsx
+++ frontend/src/features/Menu.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
-import { IonContent, IonFooter, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
+import { IonRouterOutlet } from '@ionic/react';
+import { addCircle, home, settings } from 'ionicons/icons';
+import { Redirect, Route } from 'react-router-dom';
+import { IonReactRouter } from '@ionic/react-router';
 
 function Menu() {
   return (
     <>
-      <IonHeader>
-        <IonToolbar>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-      </IonContent>
-      <IonFooter>
-        <IonToolbar>
-        </IonToolbar>
-      </IonFooter>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/home">
+            </Route>
+            <Route exact path="/add">
+            </Route>
+            <Route path="/settings">
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/home"/>
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="a">
+              <IonIcon icon={home}/>
+              <IonLabel>Home</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="b">
+              <IonIcon icon={addCircle}/>
+              <IonLabel>Add</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="c">
+              <IonIcon icon={settings}/>
+              <IonLabel>Settings</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
     </>
   );
 }
