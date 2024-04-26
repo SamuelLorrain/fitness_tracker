a160b82cfbb16be800cc0d441f8aca3f0dec9a72
SamuelLorrain <samuel.lorrain@gmail.com>
2024-04-27 00:54:23 +0200

    feat: add CORS middleware

--- backend/fitness/main.py
+++ backend/fitness/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from fitness.authentication.presentation.api import auth_router
+from fitness.commons.settings import Settings
 from fitness.food.presentation.api import food_router
 from fitness.entry.presentation.api import entry_router
 
 from fitness.commons.exceptions import CustomException
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[Settings().FRONTEND_DOMAIN],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.exception_handler(CustomException)
 async def custom_exception_handler(_: Request, exc: CustomException) -> JSONResponse:
