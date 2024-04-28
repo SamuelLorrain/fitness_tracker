512e80272bbccfee27f0db71d5713835b8ea08f8
SamuelLorrain <samuel.lorrain@gmail.com>
2024-04-28 11:48:48 +0200

    feat(authentication): persist token in storage (using @ionic/storage)

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -11,12 +11,25 @@ import { useDispatch } from 'react-redux';
 import { useLoginMutation } from '../state/api';
 import { initUser } from "../state/userSlice";
 import { useHistory } from "react-router-dom";
+import { PersistenceSingleton } from "../state/persistence";
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const dispatch = useDispatch();
   const [mutateLogin, { isLoading }] = useLoginMutation();
+
+  const onLogin = async () => {
+    try {
+      const user = await mutateLogin({username: email, password }).unwrap();
+      dispatch(initUser(user));
+      await PersistenceSingleton().set('user_email', email);
+      await PersistenceSingleton().set('user_token', user.access_token);
+      window.history.replaceState(null, null, '/');
+    } catch (e) {
+      console.log("error", e);
+    }
+  }
 
   return (
     <IonGrid>
@@ -33,11 +46,7 @@ const Login = () => {
           >
             <IonInputPasswordToggle slot="end"/>
           </IonInput>
-          <IonButton expand="full" disabled={isLoading} onClick={async () => {
-            const user = await mutateLogin({username: email, password }).unwrap();
-            dispatch(initUser(user));
-            window.history.replaceState(null, null, '/');
-          }}>
+          <IonButton expand="full" disabled={isLoading} onClick={onLogin}>
             Login
           </IonButton>
         </IonCol>
---
+++ frontend/src/state/persistence.ts
@@ -0,0 +1,11 @@
+import { Storage } from '@ionic/storage';
+
+
+let singleton = null;
+
+export const PersistenceSingleton = () => {
+  if (singleton == null) {
+    singleton = new Storage();
+  }
+  return singleton;
+}
