ba603c66d5b270466ec60905cc2c71a793bf15d8
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-01 23:05:30 +0200

    feat: define global redux timestamp instead of local one

--- frontend/src/state/userSlice.ts
+++ frontend/src/state/userSlice.ts
@@ -1,17 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { jwtDecode } from "jwt-decode";
+import { getUnixTime } from "date-fns";
 
 export interface UserState {
-  email: String,
-  token: String | null
-  isLogged: Boolean
+  email: String;
+  token: String | null;
+  isLogged: Boolean;
+  currentTimestamp: number; // Date is not serializable, so we store timestamp instead
 }
 
 const initialState: UserState = {
   email: '',
   token: null,
-  isLogged: false
+  isLogged: false,
+  currentTimestamp: getUnixTime(new Date())
 }
 
 export const userSlice = createSlice({
@@ -24,8 +27,11 @@ export const userSlice = createSlice({
       state.token = action.payload.access_token;
       state.isLogged = true;
     },
+    setTimestamp: (state, action) => {
+      state.currentTimestamp = action.payload.timestamp;
+    }
   },
 })
 
-export const { initUser } = userSlice.actions;
+export const { initUser, setTimestamp } = userSlice.actions;
 export default userSlice.reducer;
