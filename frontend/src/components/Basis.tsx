0e8fa9f3ff7fe51d4c16c37fe7bc781725961fab
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-04 00:29:48 +0200

    feat: add "on return" option on Basis component

--- frontend/src/components/Basis.tsx
+++ frontend/src/components/Basis.tsx
@@ -1,11 +1,21 @@
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon } from '@ionic/react';
+import { arrowBack } from "ionicons/icons";
 
-const Basis: React.FC = ({name, children}) => {
+const Basis: React.FC = ({name, onReturn = undefined, children}) => {
   return (
     <>
       <IonHeader>
         <IonToolbar>
           <IonTitle>{name}</IonTitle>
+          {
+            (onReturn != null) ? (
+              <IonButtons slot="start">
+                <IonButton onClick={onReturn}>
+                  <IonIcon icon={arrowBack}/>
+                </IonButton>
+              </IonButtons>
+            ) : null
+          }
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
