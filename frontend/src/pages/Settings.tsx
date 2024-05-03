d01454bc6889aec53787687257cd4c6e7de1f7a3
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-04 00:16:53 +0200

    feat(user): display calorie goals in front

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -9,12 +9,21 @@ const Settings: React.FC = () => {
     <Basis name="Settings">
       <IonCard>
         <IonCardHeader>
-          <IonCardTitle>Account informations</IonCardTitle>
+          <IonCardTitle>Account</IonCardTitle>
         </IonCardHeader>
         <IonCardContent>
           <div>{user.first_name}</div>
           <div>{user.last_name}</div>
           <div>{user.email}</div>
+        </IonCardContent>
+        <IonButton fill="clear" disabled="true">Change</IonButton>
+      </IonCard>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Goals per days</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <div>- {user.nutrition_goals_per_day.calories} kcal</div>
         </IonCardContent>
         <IonButton fill="clear" disabled="true">Change</IonButton>
       </IonCard>
