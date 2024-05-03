5c98ec4496cbe56f0cc22f6367b29595f2b4bc78
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-03 22:50:54 +0200

    feat(user): implement mongodb user repository

---
+++ backend/fitness/user/infra/mongo_db_user_repository.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from uuid import UUID
+from fitness.commons.connection import MongoDBConnection
+from fitness.user.domain.entities import User
+from fitness.user.domain.user_repository import UserRepository
+
+
+class MongoDBUserRepository(UserRepository):
+    def __init__(self):
+        self.db = MongoDBConnection().db
+        self.user_collection = self.db.user_collection
+
+    def get_user(self, user_uuid: UUID) -> Optional[User]:
+        db_user = self.user_collection.find_one({"uuid": user_uuid})
+        if db_user is None:
+            return None
+        if db_user.get("calories_goals_per_day") is None:
+            return User(**db_user)
+        return User(**db_user, calories_goals_per_day=None)
