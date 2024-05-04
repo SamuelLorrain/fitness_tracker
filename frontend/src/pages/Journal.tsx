efffaf7d239cba6cc04cd46066098b743f939e6c
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-04 12:34:05 +0200

    feat(journal): create daily progress component

--- frontend/src/pages/Journal.tsx
+++ frontend/src/pages/Journal.tsx
@@ -3,11 +3,14 @@ import { useParams, useHistory } from "react-router-dom";
 import Basis from "../components/Basis";
 import { useListEntryQuery } from "../state/api";
 import { IonButton, IonHeader, IonContent, IonToolbar, IonTitle, IonButtons, IonIcon } from "@ionic/react";
+import { IonCard, IonCardContent, IonProgressBar } from "@ionic/react";
+import { IonGrid, IonRow, IonCol } from "@ionic/react";
 import { formatDay } from "../utils/date_utils";
 import { add, format, parse, getUnixTime } from "date-fns";
 import { chevronBack, chevronForward } from "ionicons/icons";
 import { useSelector, useDispatch } from "react-redux";
 import { setTimestamp } from "../state/userSlice";
+import { safeDiv } from "../utils/math_utils";
 
 const useJournal = () => {
   const dispatch = useDispatch();
@@ -38,6 +41,105 @@ const useJournal = () => {
   }
 }
 
+const useDailyProgress = () => {
+  const caloriesGoal = useSelector(state => state.user?.nutrition_goals_per_day?.calories);
+  const proteinsGoal = useSelector(state => state.user?.nutrition_goals_per_day?.proteins.protein);
+  const lipidsGoal = useSelector(state => state.user?.nutrition_goals_per_day?.lipids.fat);
+  const carbsGoal = useSelector(state => state.user?.nutrition_goals_per_day?.carbohydrates.carbs);
+
+  const hasCaloriesGoal = (caloriesGoal != null);
+  const hasProteinsGoal = (proteinsGoal != null);
+  const hasLipidsGoal = (lipidsGoal != null);
+  const hasCarbsGoal = (carbsGoal != null);
+  const hasGoals = hasCaloriesGoal || hasProteinsGoal || hasProteinsGoal || hasCarbsGoal;
+
+  return {
+    hasGoals,
+    hasCaloriesGoal,
+    caloriesGoal,
+    hasProteinsGoal,
+    proteinsGoal,
+    hasLipidsGoal,
+    lipidsGoal,
+    hasCarbsGoal,
+    carbsGoal,
+  }
+}
+
+type DailyProgressParam = {
+  todayCalories: number,
+  todayProteins: number,
+  todayLipids: number,
+  todayCarbs: number
+};
+
+const DailyProgress: React.FC = ({ todayCalories, todayProteins, todayLipids, todayCarbs }: DailyProgressParam) => {
+  const {
+    hasGoals,
+    hasCaloriesGoal,
+    hasProteinsGoal,
+    hasLipidsGoal,
+    hasCarbsGoal,
+    caloriesGoal,
+    proteinsGoal,
+    lipidsGoal,
+    carbsGoal
+  } = useDailyProgress();
+  const caloriesRelation = todayCalories != null ? safeDiv(todayCalories, caloriesGoal) : 0;
+  const proteinsRelation = todayProteins != null ? safeDiv(todayProteins, proteinsGoal) : 0;
+  const lipidsRelation = todayLipids != null ? safeDiv(todayLipids, lipidsGoal): 0;
+  const carbsRelation = todayCarbs != null ? safeDiv(todayCarbs, carbsGoal) : 0;
+
+  if (hasGoals === false) {
+    return <></>;
+  }
+
+  return (
+    <IonCard>
+      <IonCardContent>
+        <IonGrid>
+          {
+            hasCaloriesGoal ? (
+              <IonRow>
+                  <div className="margin-left-auto">{todayCalories ?? 0} kcal / {caloriesGoal} kcal</div>
+                  <IonProgressBar value={caloriesRelation}/>
+              </IonRow>
+            )
+            : null
+          }
+          {
+            hasProteinsGoal ? (
+              <IonRow>
+                  <div className="margin-left-auto">{todayProteins ?? 0} g / {proteinsGoal} g</div>
+                  <IonProgressBar value={proteinsRelation}/>
+              </IonRow>
+            )
+            : null
+          }
+          {
+            hasLipidsGoal ? (
+              <IonRow>
+                  <div className="margin-left-auto">{todayLipids ?? 0} g / {lipidssGoal} g</div>
+                  <IonProgressBar value={lipidsRelation}/>
+              </IonRow>
+            )
+            : null
+          }
+          {
+            hasCarbsGoal ? (
+              <IonRow>
+                  <div className="margin-left-auto">{todayCarbs ?? 0} g / {carbsGoal} g</div>
+                  <IonProgressBar value={carbsRelation}/>
+              </IonRow>
+            )
+            : null
+          }
+        </IonGrid>
+      </IonCardContent>
+    </IonCard>
+  );
+}
+
 const Journal: React.FC = () => {
   const history = useHistory();
   const { date, entries, isLoading, moveForward, moveBackward } = useJournal();
@@ -66,6 +168,7 @@ const Journal: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding">
         <div className="scrollable">
+        <DailyProgress/>
         {
           (entries == null || entries.length == 0) ?
             <div>no entries for today !</div>
---
+++ frontend/src/utils/math_utils.ts
@@ -0,0 +1,5 @@
+
+export const safeDiv = (num, den) => {
+  if (den === 0) return 0;
+  return num / den;
+}
