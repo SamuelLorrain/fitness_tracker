47a85a8711166c569b654284f2714895f9c51489
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-04 14:08:50 +0200

    feat(settings): can update proteins, lipids and carbs goals

--- frontend/src/pages/GoalsForm.tsx
+++ frontend/src/pages/GoalsForm.tsx
@@ -15,20 +15,44 @@ const GoalsForm: React.FC = () => {
   const formik = useFormik({
     initialValues: {
       calories: user.nutrition_goals_per_day?.calories,
+      proteins: user.nutrition_goals_per_day?.proteins.protein,
+      lipids: user.nutrition_goals_per_day?.lipids.fat,
+      carbs: user.nutrition_goals_per_day?.carbohydrates.carbs,
     },
     validationSchema: Yup.object({
       calories: Yup.number()
           .typeError('calories must be a number')
           .required("Calories are required"),
+      proteins: Yup.number()
+          .typeError('proteins must be a number')
+          .required("Proteins are required"),
+      lipids: Yup.number()
+          .typeError('lipids must be a number')
+          .required("Lipids are required"),
+      carbs: Yup.number()
+          .typeError('carbs must be a number')
+          .required("Carbs are required"),
     }),
     onSubmit: async (data) => {
       const formattedData = {
         nutrition_goals_per_day: {
-          calories: data.calories
+          calories: data.calories,
+          proteins: {
+            protein: data.proteins
+          },
+          lipids: {
+            fat: data.lipids
+          },
+          carbohydrates: {
+            carbs: data.carbs
+          }
         }
       };
       const new_user_infos = structuredClone(user)
       new_user_infos.nutrition_goals_per_day.calories = data.calories;
+      new_user_infos.nutrition_goals_per_day.proteins.protein = data.proteins;
+      new_user_infos.nutrition_goals_per_day.lipids.fat = data.lipids;
+      new_user_infos.nutrition_goals_per_day.carbohydrates.carbs = data.carbs;
       await mutateSetUserGoals(formattedData).unwrap();
       await dispatch(setUserInfos(new_user_infos));
       history.push('/settings');
@@ -48,6 +72,30 @@ const GoalsForm: React.FC = () => {
           errorText={formik.errors.calories}
           onIonChange={(e) => formik.setFieldValue('calories', e.detail.value)}
           onBlur={(e) => formik.setFieldTouched('calories', true)}
+        />
+        <IonInput
+          className={`${formik.errors.proteins && 'ion-invalid'} ${formik.touched.proteins && 'ion-touched'} `}
+          label="proteins per day"
+          value={formik.values.proteins}
+          errorText={formik.errors.proteins}
+          onIonChange={(e) => formik.setFieldValue('proteins', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('proteins', true)}
+        />
+        <IonInput
+          className={`${formik.errors.carbs && 'ion-invalid'} ${formik.touched.carbs && 'ion-touched'} `}
+          label="carbs per day"
+          value={formik.values.carbs}
+          errorText={formik.errors.carbs}
+          onIonChange={(e) => formik.setFieldValue('carbs', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('carbs', true)}
+        />
+        <IonInput
+          className={`${formik.errors.lipids && 'ion-invalid'} ${formik.touched.lipids && 'ion-touched'} `}
+          label="lipids per day"
+          value={formik.values.lipids}
+          errorText={formik.errors.lipids}
+          onIonChange={(e) => formik.setFieldValue('lipids', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('lipids', true)}
         />
       <IonButton type="submit" expand="full" disabled={isLoading}>
         Change
