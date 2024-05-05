45e6547aab56a6eaf4c8e9bc9d2be36d3eecb478
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-05 22:48:02 +0200

    entry #dev: scaffold barcode

--- frontend/src/pages/AddEntry.tsx
+++ frontend/src/pages/AddEntry.tsx
@@ -8,7 +8,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/react";
 import { IonCard, IonCardHeader, IonCardTitle } from "@ionic/react";
 import { IonIcon } from '@ionic/react';
 import { IonRippleEffect } from '@ionic/react';
-import { add } from 'ionicons/icons';
+import { add, barcodeOutline } from 'ionicons/icons';
 
 const isSearchValid = (search: String): boolean => {
   return search !== '' && search != null;
@@ -71,6 +71,9 @@ const AddEntry: React.FC = () => {
         <IonToolbar>
           <IonTitle>Add</IonTitle>
           <IonButtons slot="end">
+            <IonButton onClick={() => history.push('/journal/barcode')}>
+              <IonIcon icon={barcodeOutline}/>
+            </IonButton>
             <IonButton onClick={() => history.push('/journal/add-food')}>
               <IonIcon icon={add}/>
             </IonButton>
