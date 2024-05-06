4fd3bfe605395397a2acd7ce80c7b7ea018fa8d6
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-06 23:11:40 +0200

    barcode: add hardware slice

---
+++ frontend/src/state/hardwareSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { Html5Qrcode } from "html5-qrcode";
+
+export interface HardwareState {
+  hasCameraPermission: boolean;
+  cameras: {
+    id: str,
+    label: str
+  }[];
+}
+
+const initialState: HardwareState = {
+  hasCameraPermission: false,
+  cameras: []
+}
+
+export const hardwareSlice = createSlice({
+  name: 'hardware',
+  initialState,
+  reducers: {
+    initCameras: (state, action) => {
+      state.hasCameraPermission = true;
+      state.cameras = action.payload;
+    }
+  }
+})
+
+export const { initCameras } = hardwareSlice.actions;
+export default hardwareSlice.reducer;
--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from "./userSlice";
+import hardwareReducer from "./hardwareSlice";
 import { api } from "./api"
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     user: userReducer,
+    hardware: hardwareReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
