b68dd6fde0b59abb37d61d0a3f458e0e25d4ff8c
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-08 17:46:36 +0200

    feat(food): implement barcode api enpoints

--- backend/fitness/food/configuration.py
+++ backend/fitness/food/configuration.py
@@ -1,10 +1,15 @@
 
 from dataclasses import dataclass
+
+import requests
 from fitness.commons.singleton import Singleton
+from fitness.food.domain.barcode_service import BarcodeService
 from fitness.food.domain.food_crud_service import FoodCrudService
+from fitness.food.domain.food_distant_client import FoodDistantClient
 from fitness.food.domain.food_repository import FoodRepository
 from functools import cached_property
 from fitness.food.infra.mongodb_food_repository import MongoDBFoodRepository
+from fitness.food.infra.open_food_facts_distant_client import OpenFoodFactsDistantClient
 
 @dataclass
 class FoodConfiguration(Singleton):
@@ -15,3 +20,11 @@ class FoodConfiguration(Singleton):
     @cached_property
     def food_repository(self) -> FoodRepository:
         return MongoDBFoodRepository()
+
+    @property
+    def food_distant_client(self) -> FoodDistantClient:
+        return OpenFoodFactsDistantClient()
+
+    @cached_property
+    def barcode_service(self) -> BarcodeService:
+        return BarcodeService(self.food_repository, self.food_distant_client)
