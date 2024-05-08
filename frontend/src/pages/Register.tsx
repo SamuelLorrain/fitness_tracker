a24038adb64d66fb0e9f512d90b99c8bdb03ec58
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-08 20:36:47 +0200

    dev(auth): add entry component

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,105 @@
+import {
+  IonButton,
+  IonInput,
+  IonInputPasswordToggle,
+  useIonToast,
+} from "@ionic/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useRegisterMutation } from "../state/api";
+import { useHistory } from "react-router";
+
+
+const Register: React.FC = () => {
+  const [mutateRegister, { isLoading }] = useRegisterMutation();
+  const history = useHistory();
+  const [presentToast] = useIonToast()
+  const formik = useFormik({
+    initialValues: {
+      first_name: null,
+      last_name: null,
+      email: null,
+      password: null,
+      confirm_password: null
+    },
+    validationSchema: Yup.object({
+      first_name: Yup.string().required("A first name must be provided"),
+      last_name: Yup.string().required("A last name must be provided"),
+      email: Yup.string().email().required("An email is required"),
+      password: Yup.string().required('A password must be provided'),
+      confirm_password: Yup.string().oneOf([Yup.ref('password'), null], "Must match password")
+    }),
+    onSubmit: async (data) => {
+      try {
+        const response = await mutateRegister(data).unwrap();
+      } catch(e) {
+        if (e?.data?.details) {
+          presentToast({
+            message: e?.data?.details,
+            position: 'bottom',
+            duration: 1500,
+          })
+        }
+      }
+      window.location.reload();
+    }
+  });
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <IonInput
+          className={`${formik.errors.first_name && 'ion-invalid'} ${formik.touched.first_name && 'ion-touched'} `}
+          label="First Name"
+          value={formik.values.first_name}
+          errorText={formik.errors.first_name}
+          onIonChange={(e) => formik.setFieldValue('first_name', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('first_name', true)}
+        />
+        <IonInput
+          className={`${formik.errors.last_name && 'ion-invalid'} ${formik.touched.last_name && 'ion-touched'} `}
+          label="Last Name"
+          value={formik.values.last_name}
+          errorText={formik.errors.last_name}
+          onIonChange={(e) => formik.setFieldValue('last_name', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('last_name', true)}
+        />
+        <IonInput
+          className={`${formik.errors.email && 'ion-invalid'} ${formik.touched.email && 'ion-touched'} `}
+          label="Email"
+          value={formik.values.email}
+          errorText={formik.errors.email}
+          onIonChange={(e) => formik.setFieldValue('email', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('email', true)}
+        />
+        <IonInput
+          type="password"
+          className={`${formik.errors.password && 'ion-invalid'} ${formik.touched.password && 'ion-touched'} `}
+          label="Password"
+          value={formik.values.password}
+          errorText={formik.errors.password}
+          onIonChange={(e) => formik.setFieldValue('password', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('password', true)}
+        >
+          <IonInputPasswordToggle slot="end"/>
+        </IonInput>
+        <IonInput
+          type="password"
+          className={`${formik.errors.confirm_password && 'ion-invalid'} ${formik.touched.confirm_password && 'ion-touched'} `}
+          label="confirm_password"
+          value={formik.values.confirm_password}
+          errorText={formik.errors.confirm_password}
+          onIonChange={(e) => formik.setFieldValue('confirm_password', e.detail.value)}
+          onBlur={(e) => formik.setFieldTouched('confirm_password', true)}
+        >
+          <IonInputPasswordToggle slot="end"/>
+        </IonInput>
+        <IonButton type="submit" expand="full" disable={isLoading}>
+          Create account
+        </IonButton>
+      </form>
+    </>
+  );
+}
+
+export default Register;
