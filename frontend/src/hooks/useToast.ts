7f475a6a903bd2dc188c0857be5eb8f749ac9842
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-09 13:54:40 +0200

    fix: add toasts for error handling

---
+++ frontend/src/hooks/useToast.ts
@@ -0,0 +1,24 @@
+import {
+  useIonToast,
+} from "@ionic/react";
+
+
+const TOAST_POSITION = 'bottom';
+const TOAST_DURATION = 1500;
+const TOAST_DEFAULT_MESSAGE = "An error occured";
+
+export const useToast = () => {
+  const [presentToast] = useIonToast();
+
+  const toast = (e) => {
+    presentToast({
+      message: `Error ${e?.data?.details ?? TOAST_DEFAULT_MESSAGE}`,
+      position: TOAST_POSITION,
+      duration: TOAST_DURATION,
+    })
+  }
+
+  return {
+    toast
+  }
+}
