82b67eb639a937805f96844be4cc4e20b4986385
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-09 13:31:24 +0200

    dev(entry): create journalCard component

---
+++ frontend/src/components/JournalCard.tsx
@@ -0,0 +1,21 @@
+
+const formatCard = (entry) => {
+  switch (entry.entry_type) {
+    case "food":
+      return `${entry.payload.food_name} - ${entry.payload.nutrition.serving_size.grams} g`;
+    case "water":
+      return `Water -  ${entry.payload.grams} g`;
+    default:
+      return `Calories -  ${entry.payload.kcal} kcal`;
+  }
+}
+
+const JournalCard = ({ entry, key }) => {
+  return (
+    <div key={key}>
+      <div>{formatCard(entry)}</div>
+    </div>
+  );
+}
+
+export default JournalCard;
