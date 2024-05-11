182588d9adb0f646e73957a132bfd60ad2836372
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-12 01:45:35 +0200

    chore: update capacitor config

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,9 +1,17 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'frontend',
-  webDir: 'dist'
+  appId: 'app.fitness.tracker.com',
+  appName: 'fitness_tracker',
+  webDir: 'dist',
+  server: {
+    cleartext: true,
+    androidScheme: 'http',
+  },
+  android: {
+    loggingBehavior: 'debug',
+    allowMixedContent: true,
+  }
 };
 
 export default config;
