359bfa376b8e9d0fc8837e587c24670731d0de27
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-12 04:46:13 +0200

    feat: create a new message toast

--- frontend/src/hooks/useToast.ts
+++ frontend/src/hooks/useToast.ts
@@ -2,7 +2,7 @@ import { useIonToast } from "@ionic/react";
 
 const TOAST_POSITION = "bottom";
 const TOAST_DURATION = 1500;
-const TOAST_DEFAULT_MESSAGE = "An error occured";
+const TOAST_DEFAULT_MESSAGE = "An error occurred";
 
 export const useToast = () => {
   const [presentToast] = useIonToast();
@@ -15,7 +15,18 @@ export const useToast = () => {
     });
   };
 
+  const messageToast = (message: string) => {
+    if (message) {
+      presentToast({
+        message: message,
+        position: TOAST_POSITION,
+        duration: TOAST_DURATION,
+      });
+    }
+  }
+
   return {
     toast,
+    messageToast
   };
 };
