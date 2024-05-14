076043d02bdc61f5d7bd8a6ce37028579ad52467
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-14 04:24:12 +0200

    dev(notification): add get_eligible user method in repository

--- backend/fitness/notification/domain/notification_repository.py
+++ backend/fitness/notification/domain/notification_repository.py
@@ -1,9 +1,14 @@
 from typing import Optional, Protocol
 from uuid import UUID
-from fitness.notification.domain.value_objects import Token
+
+from fitness.notification.domain.value_objects import NotificationReceiver, Token
 
 
 class NotificationRepository(Protocol):
     def set_token(self, user_uuid: UUID, token: str) -> None: ...  # pragma: no cover
 
     def get_token(self, user_uuid: UUID) -> Optional[Token]: ...  # pragma: no cover
+
+    def get_elligible_notification_receivers(
+        self,
+    ) -> list[NotificationReceiver]: ...  # pragma: no cover
