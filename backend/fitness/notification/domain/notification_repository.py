7ab09639b18dd010c1ec8470e4ecbf784b298f2d
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-14 12:04:39 +0200

    dev(notification): add a method to update water notification date

--- backend/fitness/notification/domain/notification_repository.py
+++ backend/fitness/notification/domain/notification_repository.py
@@ -1,6 +1,5 @@
 from typing import Optional, Protocol
 from uuid import UUID
-
 from fitness.notification.domain.value_objects import NotificationReceiver, Token
 
 
@@ -12,3 +11,7 @@ class NotificationRepository(Protocol):
     def get_elligible_notification_receivers(
         self,
     ) -> list[NotificationReceiver]: ...  # pragma: no cover
+
+    def update_water_notification_date(
+        self, user_uuid: UUID
+    ) -> None: ...  # pragma: no cover
