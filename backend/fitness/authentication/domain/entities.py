2832c6789b852637c0a4d864bb6270f21739f08e
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-16 09:01:13 +0200

    dev: add notification permissions

--- backend/fitness/authentication/domain/entities.py
+++ backend/fitness/authentication/domain/entities.py
@@ -8,6 +8,8 @@ from pydantic import BaseModel, EmailStr, Field
 class Permission(str, Enum):
     change_permissions = "change_permissions"
     create_global_food = "create_global_food"
+    send_notification_to_any_user = "send_notification_to_any_user"
+    schedule_notifications = "schedule_notifications"
 
 
 class Auth(BaseModel):
