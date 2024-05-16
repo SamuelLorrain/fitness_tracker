977179f54c25af956e160ef8061c752f7be13fc0
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-16 09:03:24 +0200

    feat(notification): add routes to send notifications

--- backend/fitness/notification/presentation/api.py
+++ backend/fitness/notification/presentation/api.py
@@ -3,12 +3,18 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status
 
 from fitness.authentication.configuration import AuthenticationConfiguration
-from fitness.authentication.domain.entities import AuthPassKey
+from fitness.authentication.domain.entities import AuthPassKey, Permission
 from fitness.notification.configuration import NotificationConfiguration
-from fitness.notification.presentation.contracts import TokenRequest
+from fitness.notification.presentation.contracts import (
+    NotificationRequest,
+    TokenRequest,
+)
 
 notification_router = APIRouter(prefix="/notification", tags=["notification"])
 auth_dep = AuthenticationConfiguration().authorisation_dependency
+auth_permission_dep = (
+    AuthenticationConfiguration().authorisation_permission_dependency_creator
+)
 
 
 @notification_router.put("/token", status_code=status.HTTP_204_NO_CONTENT)
@@ -21,7 +27,7 @@ def set_notification_token(
     service.set_token(auth_pass_key, token_request.token)
 
 
-@notification_router.get("/send-test", status_code=status.HTTP_200_OK)
+@notification_router.post("/send-test", status_code=status.HTTP_200_OK)
 def send_test_notification(
     auth_pass_key: Annotated[AuthPassKey, Depends(auth_dep)]
 ) -> None:
@@ -30,9 +36,29 @@ def send_test_notification(
     service.send_test_notification(auth_pass_key)
 
 
-# TODO M2M token of some kind ?
 @notification_router.post("/schedule", status_code=status.HTTP_200_OK)
-def schedule_notifications() -> None:
+def schedule_notifications(
+    _: Annotated[
+        AuthPassKey, Depends(auth_permission_dep(Permission.schedule_notifications))
+    ]
+) -> None:
     configuration = NotificationConfiguration()
     service = configuration.notification_service
     service.schedule_notifications()
+
+
+@notification_router.post("/send", status_code=status.HTTP_200_OK)
+def send_notification_to_user(
+    notification_request: NotificationRequest,
+    _: Annotated[
+        AuthPassKey,
+        Depends(auth_permission_dep(Permission.send_notification_to_any_user)),
+    ],
+) -> None:
+    configuration = NotificationConfiguration()
+    service = configuration.notification_service
+    service.send_notification(
+        notification_request.user_uuid,
+        notification_request.title,
+        notification_request.text,
+    )
--- backend/fitness/notification/presentation/contracts.py
+++ backend/fitness/notification/presentation/contracts.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 
 from fitness.notification.domain.value_objects import Token
@@ -5,3 +7,9 @@ from fitness.notification.domain.value_objects import Token
 
 class TokenRequest(BaseModel):
     token: Token
+
+
+class NotificationRequest(BaseModel):
+    user_uuid: UUID
+    title: str
+    text: str
